8298e81dcce46f658527975f250b89e32be49410
Mohit Sudhakar <mohit.sudhakar@gmail.com>
2015-10-10 11:52:24 +0530

    added weather api trying to setInterval for weather api ajax call

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var count = 0;
+
 function validate(streetStr, cityStr) {    
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
@@ -5,21 +7,29 @@ function validate(streetStr, cityStr) {
     if (streetStr.length == 0 && cityStr.length == 0) {
         $('#street')
         .css({
-            'border':'2px solid red'
+            'border':'1px solid red'
         });
 
         $('#city')
         .css({
-            "border":"2px solid red",
+            "border":"1px solid red",
         });
-        //add error msg for street
+        
+        //if the previous error was a onlycityerror then remove 
+        //that class, so that a fresh class can be added on error.
         $('#cityerr').removeClass('onlycityerror');
+
+        //add error msg for street
         $('#strerr')
         .css({
             "display":"inline"
         })
         .text('Street Name is required!')
         .addClass('streetcityerror');
+        
+        if(count != 0) {
+            $('#strerr').css('color','red');
+        }
 
         $('#cityerr')
         .css({
@@ -27,11 +37,16 @@ function validate(streetStr, cityStr) {
         })
         .text('City Name is required!')
         .addClass('streetcityerror');
+        
+        if(count != 0) {
+            $('#cityerr').css('color','red');    
+        }
+            
                 
         return false;
     } 
     else if (streetStr.length == 0 && cityStr.length != 0) {
-        $('#street').css('border','2px solid red');
+        $('#street').css('border','1px solid red');
         $('#city').css("border","");
         
         //add error msg for street
@@ -43,11 +58,15 @@ function validate(streetStr, cityStr) {
         $('#strerr').text('Street Name is required!');
         //add error class for street
         $('#strerr').addClass('streetcityerror');
+        
+        if(count != 0) {
+            $('#strerr').css('color','red');            
+        }
   /*      $('#street').css("color","black");*/
         return false;
     } 
     else if (streetStr.length != 0 && cityStr.length == 0) {
-        $('#city').css("border","2px solid red");
+        $('#city').css("border","1px solid red");
         $('#street').css("border","");
         
         //add error msg for city
@@ -57,6 +76,9 @@ function validate(streetStr, cityStr) {
         //add error class for street
         $('#cityerr').addClass('onlycityerror');
         
+        if(count != 0) {
+            $('#cityerr').css('color','red');            
+        }
         /*$('#city').css("color","black");*/
         return false;
     }
@@ -65,7 +87,7 @@ function validate(streetStr, cityStr) {
         $('#city').css("border","");
         $('#strerr').css("display","none");
         $('#cityerr').css("display","none");
-        
+        count = 1;
         return true;
     }
 }
@@ -74,8 +96,13 @@ function loadData() {
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
+    var $nytCity = $('#nyt-city');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $weatherHeader = $('#weather-header');
+    var $weatherInfo = $('#weather-info');
+    
+
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
@@ -87,29 +114,42 @@ function loadData() {
         var address = streetStr + ', ' + cityStr;
         $greeting.text('So, you want to live at ' + address + '?');
  
-        // load streetview
+        // LOAD STREETVIEW
+
         var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
         $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
         
-        // load nytimes
+        // LOAD NY TIMES
+
         var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ef59e7aa1625ddc16d9bd7ee12f7ec65:19:73004334';
+        
         $.getJSON(nytimesUrl, function(data){
-            $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+                //Dynamic resize of NY Times container.
+
+                var offset = $(".nytimes-container").offset();
+                var nycont_ht = window.innerHeight - offset.top - 5;
+                $('.nytimes-container').css('height', nycont_ht);
+
+                $(".nytimes-container").css("overflow", "scroll");
+
+            //Fetching data from NY Times API
+            $nytHeaderElem.text('NY Times Articles About').append('<br><p>'+cityStr+'</p>');
+
             articles = data.response.docs;
             for (var i = 0; i < articles.length; i++) {
                 var article = articles[i];
                 $nytElem.append('<li class="article">'+
                     '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                    '<p>' + article.snippet + '</p>'+
+                    '<p>' + article.snippet + '</p>'+ 
                 '</li>');
             };
-            $(".nytimes-container").css("height", "470px");
-            $(".nytimes-container").css("overflow", "scroll");
+        
         }).error(function(e){
             $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
         });
 
-        // load wikipedia data
+        // LOAD WIKIPEDIA DATA
+
         var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
         var wikiRequestTimeout = setTimeout(function(){
             $wikiElem.text("failed to get wikipedia resources");
@@ -129,11 +169,8 @@ function loadData() {
                 clearTimeout(wikiRequestTimeout);
             }
         });
-        
-    } //addon
-    else {
 
-    }
+    } //addon
 
     return false;
 };
@@ -154,9 +191,9 @@ function loadMap(){
             cache: false,
             success: function(result){
                 if(result.results.length){
-                    lat=result.results[0].geometry.location.lat;
-                    lng=result.results[0].geometry.location.lng;     
-                    var latlng = new google.maps.LatLng(lat, lng);     
+                    var lat=result.results[0].geometry.location.lat;
+                    var lon=result.results[0].geometry.location.lng;     
+                    var latlng = new google.maps.LatLng(lat, lon);     
                     var mapOptions = {
                       zoom: 12,
                       center: latlng,
@@ -171,8 +208,8 @@ function loadMap(){
                     google.maps.event.addListener(marker, 'click', function() {
                         infowindow.open(map,marker);
                     });
-                    $('#gmap').css('height','350px');
-                    $('#gmap').css('width','350px');
+                    $('#gmap').css('height','400px');
+                    $('#gmap').css('width','250px');
                 }
             }
         });
@@ -180,6 +217,51 @@ function loadMap(){
     }  //addon
 
 }
+
+function weather() {
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val(); 
+    var $weatherHeader = $('#weather-header');
+    var $weatherInfo = $('#weather-info');
+    var lat2 = 0, lon2 = 0;
+    if (validate(streetStr, cityStr)) {
+
+        //passing cityStr
+        var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityStr + "&APPID=2ef9e90f8ea043df6d3ed60bd738a919";
+
+        //callback function from ajax request
+        /*setInterval(myTimer, 1000);*/
+        $.getJSON( weatherUrl, function(response) {
+            //clear previous data
+            $('#weathernewinfo').remove();
+            //change dimensions of div
+            $('.weather-container').css('height','200px');
+            $weatherHeader.text('Weather in ' + cityStr);
+            $weatherHeader.css('color','blue');
+            description = response.weather[0].description;
+            temp = response.main.temp - 273;//subtract 273.15 to get celsius
+            var temp_roundoff = Math.round(temp * 100) / 100;
+            resplat = response.coord.lat;
+            resplon = response.coord.lon;
+            humidity = response.main.humidity;//add %
+            $weatherInfo
+            .append(
+                '<p id="weathernewinfo"><b>Temperature: '+temp_roundoff+'&deg;C</b><br>' + 
+                '<b>Humidity: '+humidity+'%</b><br>' + 
+                '<b>Description: '+description+'</b></p>'
+            );
+
+        })
+        .error(function(e){
+            $weatherHeader.text('Weather Data Could Not Be Loaded');
+        });
+
+
+
+    }
+
+}
+
 /*    
     $.getJSON({
         url: gmapUrl,
@@ -203,5 +285,6 @@ $(document).ready(function() {
 
     $('#form-container').submit(loadData);
     $('#form-container').submit(loadMap);
+    $('#form-container').submit(weather);
 
 });
